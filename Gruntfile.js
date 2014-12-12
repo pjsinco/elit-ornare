6c432e7ad3c82c05337f3ed8c7308ce3a42af4c1
pjsinco <psinco@fas.harvard.edu>
2014-12-11 19:30:29 -0600

    starts to set up svg sprites with grunticon

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,27 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-compass");
   grunt.loadNpmTasks("grunt-contrib-jade");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-svgstore");
+  grunt.loadNpmTasks("grunt-grunticon");
 
 
   grunt.initConfig({
+  
+    grunticon: {
+
+      myIcons: {
+        files: [{
+          expand: true,
+          cwd: 'builds/dev/img/svg',
+          src: ['*.svg', '*.png'],
+          dest: 'output'
+        }],
+        options: {
+        }
+      }
+
+    },
 
     compass: {
       dev: {
@@ -28,7 +46,22 @@ module.exports = function(grunt) {
           ext: '.html'
         }]
       }
-    },
+    }, // jade
+
+    copy: {
+      js: {
+        options: {
+
+        },
+        files: [{
+          expand: true,
+          cwd: 'components/js',
+          src: ['*.js'],
+          dest: 'builds/dev/js',
+          ext: '.js' 
+        }]
+      }
+    }, // copy
 
     watch: {
       options: {
@@ -43,11 +76,43 @@ module.exports = function(grunt) {
       compileHtml: {
         files: ['components/jade/*.jade'],
         tasks: ['jade']
+      },
+
+      copyJs: {
+        files: ['components/js/**/*.js'],
+        tasks: ['copy:js']
       }
-    
-    } // watch
+    }, // watch
+
+    svgstore: {
+      options: {
+        svg: {
+          viewbox: '0 0 100 100',
+          xmlns: 'http://www.w3.org/2000/svg'
+        },
+        symbol: {
+
+        },
+        formatting: {
+          indent_size: 2,
+          indent_char: ' '
+        },
+        includedemo: true
+      },
+      default: {
+        files: {
+          'builds/dev/img/*.svg': ['builds/dev/img/svg/*.svg']
+        } 
+      }
+    } // svgstore
+
   }); // initConfig
   
+  grunt.registerTask('copy-js', ['copy:js'])
+  grunt.registerTask('compile-jade', ['jade:compile'])
+  grunt.registerTask('compile-sass', ['compass:dev'])
+  grunt.registerTask('svg-grunticon', ['grunticon:myIcons']);
+  grunt.registerTask('svg-iconize', ['svgstore']);
   grunt.registerTask('default', ['watch']);
 
 }; // exports
