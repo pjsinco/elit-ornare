48b207c1cbd58bdd7e446b2bcc502cf902f266d1
pjsinco <psinco@fas.harvard.edu>
2014-12-18 10:48:38 -0600

    continues building nav

--- builds/dev/js/js/nav.js
+++ builds/dev/js/js/nav.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
   console.log('nav');
 
   $('body').addClass('js');
-  var $menu = $('#site-nav'),
-    $menulink = $('.site-nav__link--toggle');
+  var $menu = $('#site-nav'), 
+    $menulink = $('.menu-toggle');
 
-  $menulink.click(function(evt) {
-    $menulink.toggleClass('active');
-    $menu.toggleClass('active');
+  $menulink.click(function(evt) { 
+    $menulink.toggleClass('active'); 
+    $menu.toggleClass('active') ;
+    //return false;
     evt.preventDefault();
-    
   });
+
+  //$menulink.click(function(evt) {
+    //$menulink.toggleClass('active');
+    //$menu.toggleClass('active');
+    //evt.preventDefault();
+  //});
 });
